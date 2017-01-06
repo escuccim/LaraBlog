5e016f79eb9d62cf95e342a289ce7daf5046b2e8
Eric Scuccimarra <skooch@gmail.com>
2017-01-06 15:03:26 +0100

    Took out unneeded routes and fixed more path issues

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -10,10 +10,4 @@ Route::get('feed', 'FeedController@generate');
 
 /* Blog comments */
 Route::post('/blog/comment/add', 'BlogController@comment');
-
-/* Site maps */
-Route::get('sitemap', 'SiteMapController@index');
-Route::get('sitemap/blog', 'SiteMapController@blog');
-Route::get('sitemap/labels', 'SiteMapController@labels');
-Route::get('sitemap/pages', 'SiteMapController@pages');
 });
