9f3186280adadc7a2319e4356900ac818cac8828
Eric Scuccimarra <skooch@gmail.com>
2017-01-07 13:19:01 +0100

    Added some static functions to BlogClass

--- src/BlogClass.php
+++ src/BlogClass.php
@@ -2,7 +2,7 @@
 
 namespace Escuccim\LaraBlog;
 
-use Escuccim\Blog\Http\Controllers;
+use Escuccim\LaraBlog\Models\Blog;
 
 class BlogClass
 {
@@ -14,7 +14,24 @@ class BlogClass
         // constructor body
     }
 
-    public static function BlogArchives(){
+    public static function getArticle($slug){
+        $blog = Blog::where('slug', $slug)->first();
+        return $blog;
+    }
 
+    public static function getAllArticles($admin = false){
+        $blogs = Blog::getAll($admin);
+        return $blogs;
+    }
+
+    public static function getArchives(){
+        $archives = Blog::blogLinks();
+        return $archives;
+    }
+
+    public static function getComments($slug){
+        $blog = Blog::where('slug', $slug)->first();
+        $comments = $blog->comments;
+        return $comments;
     }
 }
