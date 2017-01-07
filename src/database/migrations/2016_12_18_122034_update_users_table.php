de067b54e7ce51d5318f68cfc60a2f7ff5ab6c45
Eric Scuccimarra <skooch@gmail.com>
2017-01-07 18:14:20 +0100

    Added function to migration to seed database

--- src/database/migrations/2016_12_18_122034_update_users_table.php
+++ src/database/migrations/2016_12_18_122034_update_users_table.php
@@ -18,6 +18,8 @@ class UpdateUsersTable extends Migration
     		$table->boolean('active')->default(1);
     		$table->string('image')->nullable();
     	});
+
+    	$this->seedDatabase();
     }
 
     /**
@@ -32,5 +34,33 @@ class UpdateUsersTable extends Migration
     		$table->dropColumn('type');
     		$table->dropColumn('image');
     	});
+    }
+
+    private function seedDatabase() {
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'type' => 1,
+        ]);
+
+        DB::table('tags')->insert([
+            'name' => 'test',
+        ]);
+
+        DB::table('blogs')->insert([
+            'user_id' => 1,
+            'title' => 'First Post',
+            'slug' => 'first-post',
+            'body' => 'First post. For test purposes.',
+            'published' => 1,
+            'published_at' => Carbon::now(),
+        ]);
+
+        DB::table('blog_tag')->insert([
+            'blog_id' => 1,
+            'tag_id' => 1,
+        ]);
     }
 }
