02d8f921e7f35f69b068cf49782fe0a605ac2e57
Eric Scuccimarra <skooch@gmail.com>
2017-01-09 15:33:11 +0100

    Editing tests

--- tests/BlogTest.php
+++ tests/BlogTest.php
@@ -221,28 +221,3 @@ class BlogTest extends TestCase
 		->see('Test comment');
 	}
 }
-
-
-abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
-{
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
-}
