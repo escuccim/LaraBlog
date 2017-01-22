a6c660ffa444ce8e8dd3ee1e67de15745409c31a
Eric Scuccimarra <skooch@gmail.com>
2017-01-22 15:47:19 +0100

    Updated functions a bit

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,7 @@
 if ( ! function_exists('setLanguage')) {
     function setLanguage(){
         App::setLocale( session('locale') ? session('locale') : config('app.locale'));
-        if(App::getLocale() == 'fr')
+        if (App::getLocale() != config('app.locale')) {
             setlocale(LC_TIME, 'fr_CH.UTF-8');
     }
 }
