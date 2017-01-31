b4e38c8ed5db5bb62abcfc84cc4e6f414b20a0fe
Eric Scuccimarra <skooch@gmail.com>
2017-01-31 14:39:53 +0100

    Added db migration to add comment parents

---
+++ src/database/migrations/2017_01_31_115601_update_blog_comments.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateBlogComments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blogcomments', function (Blueprint $table) {
+            $table->integer('parent_comment_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blogcomments', function (Blueprint $table) {
+            $table->dropColumn('parent_comment_id');
+        });
+    }
+}
